2728dc8e2cfc250c8077138d585db93704774744
Donald Mull Jr <dmull@djmacbook.home>
2016-05-10 19:30:23 -0400

    improving example

--- src/addressList.report.jsx
+++ src/addressList.report.jsx
@@ -1,32 +1,59 @@
 /** @jsx XSLFO.createElement */
 import XSLFO, { Component } from 'jsx-xsl-fo';
 import { Report, PageSequence, PageContent, PageHeader } from 'jsx-xsl-fo/reporting';
-import faker from 'faker';
 
-let data = (new Array(100).fill()).map(() => {
-    return {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        streetAddress: faker.address.streetAddress()
-    };
-});
+const styles = {
+    document: {
+        fontFamily: 'serif'
+    },
+    title: {
+        fontSize: '30pt'
+    }
+};
 
-//console.log(data);
+//define components as functions
+function Title(props) {
+    return <block {...styles.title}>{props.children}</block>;
+}
 
+// or as ES6 classes
 class AddressBlock extends Component {
     render() {
-        return <block>{this.props.firstName} {this.props.streetAddress}</block>;
+        return <block margin-bottom="1em" keep-together-within-page="always">
+            <block>{this.props.lastName}, {this.props.firstName}</block>
+            <block>{this.props.streetAddress}</block>
+            <block>{this.props.city}, {this.props.state} {this.props.zipCode}</block>
+        </block>;
     }
 }
 
-export default function generateReport(/* data */) {
-    return <Report>
+// Main report function
+function generateReport(data) {
+    return <Report {...styles.document}>
         <PageSequence>
+            <PageHeader>
+                <block text-align="end">Page <pageNumber /></block>
+            </PageHeader>
             <PageContent>
-                {data.map(contact => {
-                    return <AddressBlock {...contact} />;
-                })}
+                <Title>Contacts!</Title>
+                {data.contacts.map(contact => <AddressBlock {...contact} />)}
             </PageContent>
         </PageSequence>
     </Report>;
 }
+
+// Make some Test Data
+import faker from 'faker';
+let data = {
+    // make some fake contacts
+    contacts: (new Array(100).fill()).map(() => ({
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        streetAddress: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode()
+    }))
+};
+
+export default generateReport(data);
--- src/test.js
+++ src/test.js
@@ -3,14 +3,10 @@ import XSLFO from 'jsx-xsl-fo';
 import report from './addressList.report';
 //import report from './report1';
 
-const data = {
-    names: ['Bob', 'Jimbo', 'Gerry']
-};
 
-var builtReport = report(data);
 // console.log(builtReport);
 //
 // console.log(XSLFO.process(builtReport));
 
 console.log('<?xml version="1.0" encoding="UTF-8"?>');
-console.log(XSLFO.renderToString(builtReport));
+console.log(XSLFO.renderToString(report));
